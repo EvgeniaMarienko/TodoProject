98bbf6954266ccd37b61831b516bbd5e5a9a931d
Evgenia Marienko <janed0998@gmail.com>
2020-10-12 21:56:31 +0300

    add migration

---
+++ TodoDataBase/Migrations/20201012185510_FixSpelling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoDataBase.Migrations
+{
+    public partial class FixSpelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedData",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Projects",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedData",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
