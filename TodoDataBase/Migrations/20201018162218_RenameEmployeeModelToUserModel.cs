a04f639f12f81bed6697e2a01968767a94d8a5ca
Evgenia Marienko <janed0998@gmail.com>
2020-10-18 19:23:57 +0300

    Add migration RenameModel

---
+++ TodoDataBase/Migrations/20201018162218_RenameEmployeeModelToUserModel.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoDatabase.Migrations
+{
+    public partial class RenameEmployeeModelToUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Employees_EmployeeId",
+                table: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_EmployeeId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "TodoItems",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_UserId",
+                table: "TodoItems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TodoItems_Users_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TodoItems_UserId",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "TodoItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_EmployeeId",
+                table: "TodoItems",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TodoItems_Employees_EmployeeId",
+                table: "TodoItems",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
